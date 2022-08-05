da983978684450b2619ae45c2ba707fd6b1313f2
Kien <fukz2803@gmail.com>
2022-08-05 16:23:17 +0700

    update

--- project/src/main/java/com/foodei/project/controller/WebController.java
+++ project/src/main/java/com/foodei/project/controller/WebController.java
@@ -2,6 +2,7 @@ package com.foodei.project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class WebController {
@@ -11,9 +12,24 @@ public class WebController {
         return "web/index";
     }
 
+//    @GetMapping("/category/{name}")
+//    public String getCategoryPage2(@PathVariable("name") String categoryName){
+//        return "web/categories-list";
+//    }
+
+    @GetMapping("/category")
+    public String getCa(){
+        return "web/categories-list";
+    }
+
     @GetMapping("/about")
     public String getAboutPage(){
         return "web/about";
+    }
+
+    @GetMapping("/detail")
+    public String getDetailPage(){
+        return "web/single-post";
     }
 
     @GetMapping("/contact")
